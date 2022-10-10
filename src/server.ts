a3c0a0eaaf867e1bb5fe08ba11b1daf6c282d266
unknown <brokedeveloperhelp@gmail.com>
2022-10-11 01:19:38 +0200

    feat: adding application routes to server.ts file

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,11 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
+import dashboardRoutes from './handlers/dashboardRoutes';
+import productRoutes from './handlers/productRoutes';
+import userRoutes from './handlers/usersRoutes';
+import orderRoutes from './handlers/orderRoutes';
+
 const app: express.Application = express();
 
 const port = process.env.PORT || 3000;
@@ -14,6 +19,11 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+
+app.use('/api/dashboard', dashboardRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.get('/', (_: Request, res: Response) => {
   res.send('Welcome to Storefront-online store');
