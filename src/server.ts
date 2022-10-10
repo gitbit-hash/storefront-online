6417c78a05231dfe9c00fa68405d58a2d504aead
unknown <brokedeveloperhelp@gmail.com>
2022-10-11 00:16:05 +0200

    feat: configuring a simple express app

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import 'dotenv/config';
+
+const app: express.Application = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(
+    cors({
+        origin: `${process.env.ORIGIN}:${port}`,
+        optionsSuccessStatus: 200,
+    })
+);
+
+app.get('/', (_: Request, res: Response) => {
+    res.send('Welcome to Storefront-online store')
+})
+
+app.listen(port, () => {
+    console.log(`Server is up and running at http://localhost:${port}`)
+})
