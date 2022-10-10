239076ec5edcd6d6ebf6c3310a2718aedfcec7d4
unknown <brokedeveloperhelp@gmail.com>
2022-10-11 00:36:02 +0200

    chore: configuring node-postgres pooling

---
+++ src/database.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { Pool } from 'pg';
+
+const {
+  POSTGRES_HOST,
+  POSTGRES_DB,
+  POSTGRES_DB_TEST,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
+
+let client: Pool | undefined;
+
+if (ENV === 'test') {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB_TEST,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+} else {
+  client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
+
+export default client;
